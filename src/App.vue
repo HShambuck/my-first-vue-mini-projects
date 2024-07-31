d7305ee26e029d3b191598625d1ba77a624bbb7a
HShambuck <shambuckhalifax@gmail.com>
2024-07-31 11:29:43 +0000

    Project 3 - Calculator

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 // import TodoList from './components/01TodoList.vue'
-import RandomQuoteGenerator from './components/02RandomQuoteGenerator.vue'
+// import RandomQuoteGenerator from './components/02RandomQuoteGenerator.vue'
+import Calculator from './components/03Calculator.vue'
 </script>
 
 <template>
-  <TodoList />
-  <RandomQuoteGenerator />
+  <!-- <TodoList /> -->
+  <!-- <RandomQuoteGenerator /> -->
+  <Calculator />
 </template>
