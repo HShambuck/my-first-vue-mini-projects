ac968b6c299c4b1b4085579afe9343b9cb8975fa
HShambuck <shambuckhalifax@gmail.com>
2024-07-31 11:32:12 +0000

    Project 4 - Accordion

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 // import TodoList from './components/01TodoList.vue'
 // import RandomQuoteGenerator from './components/02RandomQuoteGenerator.vue'
-import Calculator from './components/03Calculator.vue'
+// import Calculator from './components/03Calculator.vue'
+import Accordion from './components/04Accordion.vue'
 </script>
 
 <template>
   <!-- <TodoList /> -->
   <!-- <RandomQuoteGenerator /> -->
-  <Calculator />
+  <!-- <Calculator /> -->
+  <Accordion />
 </template>
