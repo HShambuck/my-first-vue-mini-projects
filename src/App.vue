08ba961666fbfe0be44056e08e02c3616d5b3936
HShambuck <shambuckhalifax@gmail.com>
2024-08-06 10:14:58 +0000

    Project 5 - Form Validation

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 // import TodoList from './components/01TodoList.vue'
 // import RandomQuoteGenerator from './components/02RandomQuoteGenerator.vue'
 // import Calculator from './components/03Calculator.vue'
-import Accordion from './components/04Accordion.vue'
+// import Accordion from './components/04Accordion.vue'
+import FormValidation from './components/05FormValidation.vue'
 </script>
 
 <template>
   <!-- <TodoList /> -->
   <!-- <RandomQuoteGenerator /> -->
   <!-- <Calculator /> -->
-  <Accordion />
+  <!-- <Accordion /> -->
+  <FormValidation />
 </template>
