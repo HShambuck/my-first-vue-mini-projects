5929941a41e5f6a6e4dc78fff03bcabc5881b068
HShambuck <shambuckhalifax@gmail.com>
2024-08-06 11:19:38 +0000

    Project 6 - Password Generator

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 // import RandomQuoteGenerator from './components/02RandomQuoteGenerator.vue'
 // import Calculator from './components/03Calculator.vue'
 // import Accordion from './components/04Accordion.vue'
-import FormValidation from './components/05FormValidation.vue'
+// import FormValidation from './components/05FormValidation.vue'
+import PasswordGenerator from './components/06PasswordGenerator.vue'
 </script>
 
 <template>
@@ -11,5 +12,6 @@ import FormValidation from './components/05FormValidation.vue'
   <!-- <RandomQuoteGenerator /> -->
   <!-- <Calculator /> -->
   <!-- <Accordion /> -->
-  <FormValidation />
+  <!-- <FormValidation /> -->
+  <PasswordGenerator />
 </template>
